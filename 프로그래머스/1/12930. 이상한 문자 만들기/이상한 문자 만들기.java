e113de1a41985b0f8b6a9aea6b289c6c699a3b27
SOYEON KIM <100139289+usersy628@users.noreply.github.com>
2026-06-08 09:36:07 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.07 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        
+        int index = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            if (ch == ' ') {
+                answer.append(ch);
+                index = 0;
+            } else {
+                if (index % 2 == 0) {
+                    answer.append(Character.toUpperCase(ch));
+                } else {
+                    answer.append(Character.toLowerCase(ch));
+                }
+                
+                index++;
+            }
+        }
+        return answer.toString();
+    }
+}
